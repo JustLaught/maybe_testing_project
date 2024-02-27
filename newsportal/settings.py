6a865459110342e9c0700318dc1d8bb00f864b78
JustLaught <lemko2000@gmail.com>
2024-02-28 00:07:44 +0200

    news: Added db.sqlite3 and change db engine in setting.py

--- newsportal/settings.py
+++ newsportal/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('TEST_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
@@ -78,18 +78,18 @@ WSGI_APPLICATION = 'newsportal.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": 'newsportal_ftqj',
-        "USER": 'newsportal_ftqj_user',
-        "PASSWORD": '5L5QeaL3nPgIVk7bpTGJ2GsuZohlEN5e',
-        "HOST": 'dpg-ckeqgcfs0fgc73boikj0-a.frankfurt-postgres.render.com',
-        "PORT": '5432'
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+    # "default": {
+    #     "ENGINE": "django.db.backends.postgresql",
+    #     "NAME": 'newsportal_ftqj',
+    #     "USER": 'newsportal_ftqj_user',
+    #     "PASSWORD": '5L5QeaL3nPgIVk7bpTGJ2GsuZohlEN5e',
+    #     "HOST": 'dpg-ckeqgcfs0fgc73boikj0-a.frankfurt-postgres.render.com',
+    #     "PORT": '5432'
+    # }
 }
 
 
